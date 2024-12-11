3ccdbc57278c3dadf04f3f2259fdbd88e6b58d69
Rafael Duarte Daltio <m320000@alunos.ipb.pt>
2024-12-11 13:29:59 +0000

    inclusao método de atualizar usuário

--- src/main/java/com/rdaltio/usuario/business/UsuarioService.java
+++ src/main/java/com/rdaltio/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.rdaltio.usuario.infrastructure.entity.Usuario;
 import com.rdaltio.usuario.infrastructure.exceptions.ConflictException;
 import com.rdaltio.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.rdaltio.usuario.infrastructure.repository.UsuarioRepository;
+import com.rdaltio.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -47,5 +49,18 @@ public class UsuarioService {
 
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteUserByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        String email = jwtUtil.extractUsername(token.substring(7));
+
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFoundException("Email não localizado"));
+
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 }
